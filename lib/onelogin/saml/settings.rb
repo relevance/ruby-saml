adebf4dd93ab46483928ccfe88321f1ecdfcd778
Alex Redington and Larry Karnowski <pair@thinkrelevance.com>
2010-11-10 12:45:18 -0500

    Accomodating using config files for settings, including: IDP metadata, SP metadata, or an SP config yaml file.

--- lib/onelogin/saml/settings.rb
+++ lib/onelogin/saml/settings.rb
@@ -1,12 +1,155 @@
+require 'rexml/document'
+require 'openssl/x509'
+require 'digest/sha1'
+
 module Onelogin::Saml
   class Settings
-    attr_accessor :assertion_consumer_service_url
-    attr_accessor :issuer
+    attr_writer :assertion_consumer_service_url
+    attr_writer :issuer
     attr_accessor :sp_name_qualifier
-    attr_accessor :idp_sso_target_url
-    attr_accessor :idp_cert_fingerprint
-    attr_accessor :name_identifier_format
+    attr_writer :idp_sso_target_url
+    attr_writer :idp_cert_fingerprint
+    attr_writer :name_identifier_format
     attr_accessor :private_key
     attr_accessor :private_key_password
+
+    def idp_metadata=(metadata)
+      if metadata.respond_to?(:read)
+        @idp_metadata = metadata.read
+      elsif File.exists?(metadata)
+        File.open(metadata) { |file| @idp_metadata = file.read }
+      else
+        @idp_metadata = metadata
+      end
+    end
+
+    def sp_metadata=(metadata)
+      if metadata.respond_to?(:read)
+        @sp_metadata = metadata.read
+      elsif File.exists?(metadata)
+        File.open(metadata) { |file| @sp_metadata = file.read }
+      else
+        @sp_metadata = metadata
+      end
+    end
+
+    def sp_yaml=(yaml)
+      if yaml.respond_to?(:read)
+        @sp_yaml = yaml.read
+      elsif File.exists?(yaml)
+        File.open(yaml) { |file| @sp_yaml = file.read}
+      else
+        @sp_yaml = yaml
+      end
+    end
+
+    def idp_sso_target_url
+      @idp_sso_target_url || idp_sso_target_url_from_metadata
+    end
+
+    def idp_cert_fingerprint
+      @idp_cert_fingerprint || idp_cert_fingerprint_from_metadata
+    end
+
+    def issuer
+      @issuer || issuer_from_config
+    end
+
+    def assertion_consumer_service_url
+      @assertion_consumer_service_url || assertion_consumer_service_url_from_config
+    end
+
+    def name_identifier_format
+      @name_identifier_format || name_identifier_format_from_config
+    end
+
+    private
+
+    SSO_ELEMENT_PATH = "/EntityDescriptor/IDPSSODescriptor/SingleSignOnService[@Binding='urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect']"
+    X509_CERT_PATH = "/EntityDescriptor/IDPSSODescriptor/KeyDescriptor/ds:KeyInfo/ds:X509Data/ds:X509Certificate"
+    ENTITY_DESCRIPTOR_PATH = "/EntityDescriptor"
+    ASSERTION_CONSUMER_PATH = "/EntityDescriptor/SPSSODescriptor/AssertionConsumerService[@Binding='urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST']"
+    NAME_IDENTIFIER_PATH = "/EntityDescriptor/SPSSODescriptor/NameIDFormat"
+
+    def idp_metadata_doc
+      @idp_metadata_doc ||= REXML::Document.new(@idp_metadata)
+    end
+
+    def sp_metadata_doc
+      @sp_metadata_doc ||= REXML::Document.new(@sp_metadata)
+    end
+
+    def parsed_sp_yaml
+      @parsed_sp_yaml ||= YAML::load(@sp_yaml)
+    end
+
+    def name_identifier_format_from_config
+      if @sp_metadata
+        name_identifier_format_from_metadata
+      elsif @sp_yaml
+        name_identifier_format_from_yaml
+      end
+    end
+
+    def name_identifier_format_from_yaml
+      format_val = parsed_sp_yaml["name_id_format"]
+      if format_val.respond_to?(:first)
+        format_val.first
+      else
+        format_val
+      end
+    end
+    
+    def name_identifier_format_from_metadata
+      name_identifier_element = sp_metadata_doc.elements[NAME_IDENTIFIER_PATH]
+      name_identifier_element.text
+    end
+
+    def assertion_consumer_service_url_from_config
+      if @sp_metadata
+        assertion_consumer_service_url_from_metadata
+      elsif @sp_yaml
+        assertion_consumer_service_url_from_yaml
+      end
+    end
+
+    def assertion_consumer_service_url_from_yaml
+      parsed_sp_yaml["consumer_url"]
+    end
+
+    def assertion_consumer_service_url_from_metadata
+      assertion_consumer_element = sp_metadata_doc.elements[ASSERTION_CONSUMER_PATH]
+      assertion_consumer_element.attribute('Location').value
+    end
+
+    def issuer_from_config
+      if @sp_metadata
+        issuer_from_metadata
+      elsif @sp_yaml
+        issuer_from_yaml
+      end
+    end
+
+    def issuer_from_yaml
+      parsed_sp_yaml["issuer"]
+    end
+
+    def issuer_from_metadata
+      entity_descriptor_element = sp_metadata_doc.elements[ENTITY_DESCRIPTOR_PATH]
+      entity_descriptor_element.attribute('entityID').value
+    end
+
+    def idp_cert_fingerprint_from_metadata
+      base64_cert = idp_metadata_doc.elements[X509_CERT_PATH].text
+      cert_text = Base64.decode64(base64_cert)
+      cert = OpenSSL::X509::Certificate.new(cert_text)
+      fingerprint = Digest::SHA1.hexdigest(cert.to_der)
+    end
+
+    def idp_sso_target_url_from_metadata
+      sso_service_element = idp_metadata_doc.elements[SSO_ELEMENT_PATH]
+      sso_service_element.attribute('Location').value
+    end
+    
   end
 end
---
+++ spec/onelogin/saml/settings_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe Onelogin::Saml::Settings do
+  
+  let(:settings) do
+    settings = Onelogin::Saml::Settings.new
+  end
+
+  describe "idp_metadata" do
+    
+    before(:each) do
+      settings.idp_metadata = File.dirname(__FILE__) + "/../../fixtures/idp.xml"
+    end
+    
+    it "pulls idp_sso_target_url from the idp metadata" do
+      settings.idp_sso_target_url.should == "http://saml.example.com/login"
+    end
+    
+    it "pulls idp_cert_fingerprint from the idp metadata" do
+      settings.idp_cert_fingerprint.should == "def18dbed547cdf3d52b627f41637c443045fe33"
+    end
+  end
+
+  describe "sp_metadata" do
+    
+    before(:each) do
+      settings.sp_metadata = File.dirname(__FILE__) + "/../../fixtures/sp.xml"
+    end
+
+    it "pulls issuer from the sp metadata" do
+      settings.issuer.should == "http://sp.example.com"
+    end
+
+    it "pulls assertion_consumer_service_url from the sp metadata" do
+      settings.assertion_consumer_service_url.should == "http://sp.example.com/session"
+    end
+
+    it "defaults name_identifier_format from the sp metadata if unset" do
+      settings.name_identifier_format.should == "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
+    end
+
+    it "overrides name_identifier_format with the explicitly set value if set" do
+      settings.name_identifier_format = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+      settings.name_identifier_format.should == "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+    end
+        
+  end
+
+  describe "sp_yaml" do
+
+    before(:each) do
+      settings.sp_yaml = File.dirname(__FILE__) + "/../../fixtures/sp.yaml"
+    end
+
+    it "pulls issuer from the sp yaml" do
+      settings.issuer.should == "http://sp.example.com"
+    end
+    
+    it "pulls assertion_consumer_service_url from the sp yaml" do
+      settings.assertion_consumer_service_url.should == "http://sp.example.com/session"
+    end
+    
+    it "defaults name_identifier_format from the sp yaml if unset" do
+      settings.name_identifier_format.should == "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
+    end
+    
+    it "overrides name_identifier_format with the explicitly set value if set" do
+      settings.name_identifier_format = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+      settings.name_identifier_format.should == "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+    end
+
+  end
+  
+end 
