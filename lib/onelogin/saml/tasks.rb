7b001126eb9afa6f9125026c9dc0f21de4da5bfc
Alex Redington and Larry Karnowski <pair@thinkrelevance.com>
2010-11-11 13:48:09 -0500

    Add error checking and output messages

--- lib/onelogin/saml/tasks.rb
+++ lib/onelogin/saml/tasks.rb
@@ -40,6 +40,11 @@ module Onelogin
         issuer = parsed_yaml["issuer"]
         consumer_url = parsed_yaml["consumer_url"]
         name_id_format = parsed_yaml["name_id_format"]
+        unless issuer && consumer_url && name_id_format
+          needed_keys = ["issuer", "consumer_url", "name_id_format"]
+          puts "sp.yml does not include #{needed_keys - parsed_yaml.keys}, correct before generating metadata"
+          return
+        end
         cert_file = parsed_yaml["cert_file"]
         if cert_file
           cert_text = cert_from_filename(cert_file)
@@ -49,6 +54,7 @@ module Onelogin
         erb = ERB.new(template,0,"%<>>")
         output = erb.result(binding)
         File.open("./config/sp.xml","w") { |f| f.write output }
+        puts "Wrote ./config/sp.xml"
       end
       
       def gen_cert
@@ -57,21 +63,6 @@ module Onelogin
         system 'openssl req -new -x509 -days 1001 -key ./config/saml_certs/saml.key -out ./config/saml_certs/saml.cer'
       end
       
-      # def gen_cert_ruby
-      #   #Expands to "/C=US/ST=Some-State/O=Internet Widgits Pty Ltd/emailAddress=demo@example.com"
-      #   magic_der = "0f1\v0\t\x06\x03U\x04\x06\x13\x02US1\x130\x11\x06\x03U\x04\b\x13\nSome-State1!0\x1F\x06\x03U\x04\n\x13\x18Internet Widgits Pty Ltd1\x1F0\x1D\x06\t*\x86H\x86\xF7\r\x01\t\x01\x16\x10demo@example.com"
-        
-      #   puts "Generating keys..."
-      #   cert, private_key = WEBrick::Utils.create_self_signed_cert(1024,magic_der,"Convenient fake certificate.")
-        
-      #   mkdir_p "./config/saml_certs"
-        
-      #   File.open("./config/saml_certs/saml.cer", "w") {|f| f.write(cert) }
-      #   puts "Wrote public certificate to ./config/saml_certs/saml.cer"
-        
-      #   File.open("./config/saml_certs/saml.key", "w") {|f| f.write(private_key) }
-      #   puts "Wrote private key to ./config/saml_certs/saml.key"
-      # end
     end
   end
 end
