2f3a06e9707f07c47fdeaa5e78b10d59fc5af523
Sam Umbach and Larry Karnowski <pair@thinkrelevance.com>
2010-11-19 09:16:11 -0500

    Simplified the SAML request XML
      - did NOT change the semantics of the XML document, just removed duplication

--- lib/onelogin/saml/authrequest.rb
+++ lib/onelogin/saml/authrequest.rb
@@ -14,13 +14,17 @@ module Onelogin::Saml
     def create(settings, params = {})
       issue_instant = Onelogin::Saml::Authrequest.getTimestamp
 
-      request = 
-        "<samlp:AuthnRequest xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" ID=\"#{transaction_id}\" Version=\"2.0\" IssueInstant=\"#{issue_instant}\" ProtocolBinding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" AssertionConsumerServiceURL=\"#{settings.assertion_consumer_service_url}\">" +
-        "<saml:Issuer xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">#{settings.issuer}</saml:Issuer>\n" +
-        "<samlp:NameIDPolicy xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Format=\"#{settings.name_identifier_format}\" AllowCreate=\"true\"></samlp:NameIDPolicy>\n" +
-        "<samlp:RequestedAuthnContext xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Comparison=\"exact\">" +
-        "<saml:AuthnContextClassRef xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport</saml:AuthnContextClassRef></samlp:RequestedAuthnContext>\n" +
-        "</samlp:AuthnRequest>"
+      request = <<-EOF.gsub(/^\s*/, "")
+        <AuthnRequest xmlns="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="#{transaction_id}" Version="2.0" IssueInstant="#{issue_instant}" ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" AssertionConsumerServiceURL="#{settings.assertion_consumer_service_url}">
+          <saml:Issuer>#{settings.issuer}</saml:Issuer>
+          <NameIDPolicy Format="#{settings.name_identifier_format}" AllowCreate="true"/>
+          <RequestedAuthnContext xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" Comparison="exact">
+            <saml:AuthnContextClassRef xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">
+              urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport
+            </saml:AuthnContextClassRef>
+          </RequestedAuthnContext>
+        </AuthnRequest>
+        EOF
 
       @logger.debug("Raw SAML request: #{request}") unless @logger.nil?
       
